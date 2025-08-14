e296ce5e0a3947f4666cdc0384bdcdf6ae6fa23a
Koli <2423895@cognizant.com>
2025-08-14 15:32:33 +0530

    route of login and sigup done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md relative">
+       
+
         <h2 className="text-2xl font-bold mb-6 text-center text-[#11383c]">Login to ElectroMart</h2>
         <form>
           <div className="mb-4">
